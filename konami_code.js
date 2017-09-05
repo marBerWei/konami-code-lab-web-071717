e0093c19168b8c311a4170b08b70ed17bcfe5622
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-09-05 17:51:25 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index=0
+  document.body.addEventListener('keydown', function(element){
+    const key = parseInt(element.detail || element.which)
+    if (code[index] === key){
+      index++;
 
-}
+      if (index === code.length){
+      alert("Success!")
+      }
+    } else{
+        index = 0
+    }
+  })
+  }
